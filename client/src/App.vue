c66947ee803fe0b37384abce7ae3f6e3e3243217
kazawan <55086403@qq.com>
2024-02-04 17:27:30 +0800

    modified:   client/src/App.vue 	new file:   client/src/composable/ApiClient.js 	deleted:    client/src/composable/api.js 	modified:   package.json 	modified:   server/prisma/dev.db

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,13 @@
 <template>
 SignUp 
     <label for="exampleInputName">Name</label>
-    <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp" placeholder="Enter Name" v-model="SignUp.name">
+    <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp" placeholder="Enter Name" v-model="SignUp_data.name">
     
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="SignUp.email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="SignUp_data.email">
     
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="SignUp.password">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="SignUp_data.password">
   <button @click="signup_action">SignUp</button>
 
 
@@ -15,9 +15,11 @@ SignUp
 
 <script setup>
 import { ref,reactive,onMounted }  from 'vue'
-import authFetch from './composable/api';
+import  ApiClient from './composable/ApiClient'
+const { authFetch,SignUp } = ApiClient()
 
-const SignUp = reactive({
+
+const SignUp_data = reactive({
   name:'bobo',
   email:'222@qq.com',
   password:'123123'
@@ -25,16 +27,10 @@ const SignUp = reactive({
 
 const signup_action = async() =>{
   
-  const query = {
-    data:{
-      name:SignUp.name,
-      email:SignUp.email,
-      password:SignUp.password
-    }
-  }
-  // console.log(query)
-  const res = await authFetch('SignUp',query)
+  
+  const res = await SignUp(SignUp_data)
   alert(res.msg)
+
 }
 
 
---
+++ client/src/composable/ApiClient.js
@@ -0,0 +1,77 @@
+// const fetchurl = "http://localhost:3000/";
+// const option = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// };
+
+// const authFetch = async (fnname, query) => {
+//   const res = await fetch(fetchurl + "auth", {
+//     ...option,
+//     body: JSON.stringify({ fn: fnname, query }),
+//   }).then((res) => res.json());
+//   return res;
+// };
+
+// const apiFetch = async (fnname, query) => {
+//   const res = await fetch(fetchurl + "api", {
+//     ...option,
+//     body: JSON.stringify({ fn: fnname, query }),
+//   }).then((res) => res.json());
+//   return res;
+// };
+
+// export default authFetch;
+
+const fetchurl = "http://localhost:3000/";
+const option = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export default  function ApiClient(){
+  const authFetch = async (fnname, query) => {
+    const res = await fetch(fetchurl + "auth", {
+      ...option,
+      body: JSON.stringify({ fn: fnname, query }),
+    }).then((res) => res.json());
+    return res;
+  };
+
+  const apiFetch = async (fnname, query) => {
+    const res = await fetch(fetchurl + "api", {
+      ...option,
+      body: JSON.stringify({ fn: fnname, query }),
+    }).then((res) => res.json());
+    return res;
+  };
+  
+
+  /**
+   * @name EasyAuth 登陆接口
+   * @param {object} data //一个对象
+   * @param {string} data.email //邮箱
+   * @param {string} data.password //密码
+   * @param {string} data.name //用户名
+   * 
+   * @returns 
+   */
+  const SignUp = async (data) => {
+    const fnname = 'SignUp'
+    const query = {
+      data:{
+        ...data
+      }
+    }
+    const res = await fetch(fetchurl + "auth", {
+      ...option,
+      body: JSON.stringify({ fn: fnname, query }),
+    }).then((res) => res.json());
+    return res;
+  }
+
+  return {authFetch, apiFetch,SignUp}
+}
